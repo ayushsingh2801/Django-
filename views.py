e622ad94a60facd0ecaa82f670e89e32f4e6af94
ayushsingh2801 <ayush.singh_btech23@gsv.ac.in>
2024-12-17 19:43:16 +0530

    Main app files

---
+++ views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+def home(request):
+    # return HttpResponse("Hello,World. You are at home page")
+    return render(request, 'website/index.html')
+
+def about(request):
+    return render(request, 'website/about.html')
+def contact(request):
+    return render(request, 'website/contact.html')
